977b96cee6ea542053cfbbc059706f7f8272f602
Wizerdy <grancru2000@gmail.com>
2019-09-30 12:22:44 +0200

    Layer masks

---
+++ Assets/Scripts/Flo/Camera_Mirage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Mirage : MonoBehaviour
+{
+    public LayerMask _mirage;
+
+    private Camera _cam;
+
+    void Start()
+    {
+        _cam = GetComponent<Camera>();
+
+        _cam.cullingMask &= ~_mirage;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+            SwitchLayers(_mirage);
+    }
+
+    private void SwitchLayers(int layer)
+    {
+        //if((_cam.cullingMask & layer) > 0)
+        //    _cam.cullingMask &= ~layer;
+        //else
+        //    _cam.cullingMask |= layer;
+
+        _cam.cullingMask ^= layer;
+    }
+}
---
+++ Assets/Scripts/Flo/Inventory.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public struct Struct
+    {
+        int Id;
+    }
+
+    public List<Struct> _inventory;
+
+    private void Start()
+    {
+        _inventory = new List<Struct>();
+    }
+
+    #region UtilityFunction
+    public void Show()
+    {
+        if(!gameObject.activeSelf)
+            gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false);
+    }
+
+    public void ShowOrHide()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void Add(Struct item)
+    {
+        _inventory.Add(item);
+    }
+
+    public void Remove(int id)
+    {
+        _inventory.RemoveAt(id);
+    }
+    #endregion
+}
