e7f0c60bccf024b9052753995b430d771cca4189
Nagdalam <chariou.benjamin@gmail.com>
2019-09-30 10:39:56 +0200

    Merge

---
+++ Assets/Benjamin_Microsoft/Scripts/DialogDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogDisplay : MonoBehaviour
+{
+
+    public Dialog dialog;
+
+    public Text UIName;
+    public Text UISpeech;
+    
+    public Image UIFace;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIName.text = dialog.charName;
+        UISpeech.text = dialog.charSpeech;
+
+        UIFace.sprite = dialog.charFace;
+    }
+
+}
