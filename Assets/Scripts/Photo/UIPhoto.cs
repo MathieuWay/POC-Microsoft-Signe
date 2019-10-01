586a65f5dc4b312adecb429c0ace0d66e1d3bff1
mathieuway91 <mathieu.way91@gmail.com>
2019-10-01 10:55:11 +0200

    UI Photo

---
+++ Assets/Scripts/Photo/UIPhoto.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPhoto : MonoBehaviour
+{
+    private List<Texture> screenshots = new List<Texture>();
+    private GameObject ui;
+    private void Start()
+    {
+        ui = transform.GetChild(0).gameObject;
+    }
+    public void ToogleUI()
+    {
+        ui.gameObject.SetActive(!ui.gameObject.activeSelf);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            ToogleUI();
+        }
+    }
+
+    public void NewPhoto(Texture2D photo)
+    {
+        screenshots.Add(photo);
+    }
+}
