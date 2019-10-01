d35c93bf98c7445a33909fc84329a5d00a914d4e
mathieuway91 <mathieu.way91@gmail.com>
2019-10-01 11:05:34 +0200

    UI Photo instance & prefabs canvas photo

--- Assets/Scripts/Photo/UIPhoto.cs
+++ Assets/Scripts/Photo/UIPhoto.cs
@@ -6,6 +6,15 @@ public class UIPhoto : MonoBehaviour
 {
     private List<Texture> screenshots = new List<Texture>();
     private GameObject ui;
+    private static UIPhoto instance = null;
+    public static UIPhoto Instance()
+    {
+        if (instance == null)
+            instance = GameObject.FindObjectOfType<UIPhoto>();
+        if (instance == null)
+            Debug.LogError("No UIPhoto in the scene");
+        return instance;
+    }
     private void Start()
     {
         ui = transform.GetChild(0).gameObject;
@@ -19,7 +28,7 @@ public class UIPhoto : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            ToogleUI();
+            UIPhoto.Instance().ToogleUI();
         }
     }
 
